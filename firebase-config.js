64d9fcba44d2a6e5138ebd4637bdb0ce214305d9
Imam Hossain <rsimam100@gmail.com>
2026-01-17 12:47:50 +0600

    Firebase integration complete and admin logic added

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiGEnhmJZ5Jzdk5wyIxVniD0BVTAjwrw8",
-  authDomain: "operating-b79c5.firebaseapp.com",
-  projectId: "operating-b79c5",
-  storageBucket: "operating-b79c5.firebasestorage.app",
-  messagingSenderId: "24877746514",
-  appId: "1:24877746514:web:382cd0c381ca6e0bd3928b"
+  apiKey: "AIzaSyA2fbxZ8Ja6YdjXg46uJi-BibydbWQ2jy0",
+  authDomain: "it-s-a-deal-6b8ce.firebaseapp.com",
+  projectId: "it-s-a-deal-6b8ce",
+  storageBucket: "it-s-a-deal-6b8ce.firebasestorage.app",
+  messagingSenderId: "1099258516686",
+  appId: "1:1099258516686:web:3d70989d63baf6a3a72c9f"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
